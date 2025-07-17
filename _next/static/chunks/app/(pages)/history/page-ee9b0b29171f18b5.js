cce94248d01efdf07bf51eb96b5ae93957d952c9
Ngogoyo Michael <michaeln1486@gmail.com>
2025-07-17 15:59:51 +0300

    Initial

---
+++ _next/static/chunks/app/(pages)/history/page-ee9b0b29171f18b5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{8990:function(n,e,u){Promise.resolve().then(u.bind(u,9473))}},function(n){n.O(0,[345,989,971,117,744],function(){return n(n.s=8990)}),_N_E=n.O()}]);
